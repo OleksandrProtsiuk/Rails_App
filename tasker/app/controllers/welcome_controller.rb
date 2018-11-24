b9de3337f0ccf7ed9e910acfbb2b9dbdb9a3def3
alex <alex_is_ok@yahoo.com>
2018-11-24 08:53:55 +0200

    beta ver.

---
+++ tasker/app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+require 'tasks'
+require 'conditions'
+
+class WelcomeController < ApplicationController
+  def index
+    @conditions = CONDITIONS
+  end
+
+  def task
+    @task = Tasks::MathExercises.new
+    @number = params[:id]
+  end
+end
